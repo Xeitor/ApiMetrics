3e57b06b7ed518ae4e51a3393a678cec28f2bf4b
xeitor <salaseze@gmail.com>
2020-07-18 11:13:26 -0300

    added fragments

---
+++ app/src/main/java/com/example/wisproapi/activities/slideshow/SlideShowFragment.kt
@@ -0,0 +1,24 @@
+package com.example.wisproapi.activities.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.wisproapi.R
+
+class SlideShowFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        return root
+    }
+}
