8b3a90e02fb34359168889e91c1a9d08c8f59472
xeitor <salaseze@gmail.com>
2020-07-21 21:16:13 -0300

    transition with listeners working

--- app/src/main/java/com/example/wisproapi/activities/home/HomeFragment.kt
+++ app/src/main/java/com/example/wisproapi/activities/home/HomeFragment.kt
@@ -3,17 +3,19 @@ package com.example.wisproapi.activities.home
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.*
+import androidx.viewpager.widget.ViewPager
 import com.example.wisproapi.R
 import com.example.wisproapi.activities.MyRecyclerViewAdapter
-import com.example.wisproapi.retrofit_models.PaymentHandler
 import com.example.wisproapi.viewmodels.PaymentsViewModel
-import com.google.android.material.tabs.TabLayout
+
 
 class HomeFragment : Fragment() {
 
@@ -27,11 +29,38 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.statuscode)
 
-        var tabLayout:TabLayout = root.findViewById(R.id.tablayout)
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"))
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"))
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"))
+        val sceneRoot: ViewGroup = root.findViewById(R.id.scene_root)
+        val aScene: Scene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_a, this.requireContext())
+        val anotherScene: Scene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_b, this.requireContext())
 
+        var fadeTransition: Transition = Fade()
+
+        var fadeTransition2: Transition =
+            TransitionInflater.from(this.requireContext())
+                .inflateTransition(R.transition.fade_transition)
+
+//        val a:ViewGroup = root.findViewById(R.id.scene_root)
+//        val b:ViewGroup = a.findViewById(R.id.test111)
+//        val c:ViewGroup = a.findViewById(R.id.test222)
+//        sceneRoot.setOnClickListener {
+//            TransitionManager.go(anotherScene, fadeTransition)
+//        }
+
+        //Transition listeners
+        sceneRoot.setOnClickListener {
+            TransitionManager.go(anotherScene, fadeTransition)
+        }
+
+        anotherScene.setEnterAction{
+            sceneRoot.setOnClickListener{
+                TransitionManager.go(aScene, fadeTransition)
+            }
+        }
+        aScene.setEnterAction{
+            sceneRoot.setOnClickListener{
+                TransitionManager.go(anotherScene, fadeTransition)
+            }
+        }
 
         val view_model: PaymentsViewModel by viewModels()
         view_model.get_live_payment().observe(viewLifecycleOwner, androidx.lifecycle.Observer { new->
@@ -43,12 +72,6 @@ class HomeFragment : Fragment() {
             recyclerView.adapter = adapter
 
             textView.append("Total: " + view_model.livePayment.value!!.total.toString())
-//
-//            val dividerItemDecoration = DividerItemDecoration(
-//                recyclerView.context,
-//                this.getResources().getConfiguration().orientation
-//            )
-//            recyclerView.addItemDecoration(dividerItemDecoration)
         })
         return root
     }
