57108e7c9dfab43a23e46ed07ecaeff737b84a8c
xeitor <salaseze@gmail.com>
2020-07-31 20:29:46 -0300

    some styles in ajustes

--- app/src/main/java/com/example/wisproapi/activities/slideshow/SlideShowFragment.kt
+++ app/src/main/java/com/example/wisproapi/activities/slideshow/SlideShowFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.wisproapi.R
 import com.google.android.material.snackbar.Snackbar
@@ -31,8 +32,8 @@ class SlideShowFragment : Fragment() {
         var prefs: SharedPreferences = requireActivity().getSharedPreferences("isp_information", Context.MODE_PRIVATE)
         var isp_id = prefs.getString("isp_id", "") //"No name defined" is the default value.
 
-        var editText: EditText = root.findViewById(R.id.editText1)
-        if (isp_id!!.isEmpty()) editText.text = isp_id
+        var editText: TextView = root.findViewById(R.id.isp_id_actual)
+        editText.text = "Token actual: $isp_id"
 
         button.setOnClickListener {
             btnValidateInputClick(root)
